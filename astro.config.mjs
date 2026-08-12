8e75c3117c55013a89987d5cd3e9d74f02e14930
José Viana <joscorvi@gmail.com>
2026-08-12 09:22:38 -0300

    ajuste mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
-// TODO: troque SEU_USUARIO e NOME_DO_REPO abaixo pelos valores reais assim que criar o repositório.
-// Se o repo se chamar "SEU_USUARIO.github.io", o site fica na raiz e `base` deve ser removido.
 export default defineConfig({
-	site: 'https://SEU_USUARIO.github.io',
-	base: '/NOME_DO_REPO',
+	site: 'https://jcorreaviana.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
